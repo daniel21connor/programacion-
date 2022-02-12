e66e61c73789176ba6cc3f2977ae9804209dc7b9
connor <jconnorr1@miumg.edu.gt>
2022-02-12 10:53:47 -0600

    se sube el ejercicio

---
+++ ejercicio 1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+int n1,n2, suma= 0, resta = 0, multiplicacion =0, division=0;
+
+cout<<"escriba el primer numero: "; cin>> n1;
+cout<< "digita el segundo numero: "; cin>>n2;
+suma = n1 + n2;
+resta = n1 - n2;
+multiplicacion= n1 * n2;
+division= n1 / n2;
+ 
+ cout<< "\n la suma es: " <<suma;
+  cout<< "\n la resta es: " <<resta;
+   cout<< "\n la multiplicacion  es: " <<multiplicacion;
+    cout<< "\n la division es: " <<division;
+
+
+
+
+
+return 0;
+
+
+}
+
